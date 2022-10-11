812f148964503a0b5f4067aa7a28d5d639593fa0
Veneta <36812226+veneta13@users.noreply.github.com>
2022-10-11 11:04:53 +0300

    Add week 2 - task 1
    Add

---
+++ week2_1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<fcntl.h>
+
+char buf[16];
+int main() {
+        int file = open("aa.txt", O_RDONLY);
+        int i;
+        for (i = 5; i > 0; i--) {
+                read(file, buf, i);
+                printf("%s\n", buf);
+        }
+        return 0;
+}
+
+/*
+abcde
+fghie
+jklie
+mnlie
+onlie
+*/
