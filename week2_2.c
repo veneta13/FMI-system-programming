c0ba90cc4792344a7e67ae50374eb659c8aa3c0b
Veneta <36812226+veneta13@users.noreply.github.com>
2022-10-18 10:39:18 +0300

    Update week 2 - task 2

--- week2_2.c
+++ week2_2.c
@@ -1,15 +1,17 @@
-#include<stdio.h>
-#include<fcntl.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
 
-char buf[16];
-int main() {
-        int file = open("aa.txt", O_RDONLY);
-        int i;
-        for (i = 5; i > 0; i--) {
-                read(file, buf, i);
-                write(1, buf, i);
-        }
-        return 0;
+char buffer[16];
+int main()
+{
+    int fd=open("aa",O_RDONLY);
+    int i;
+    for(i=5;i>0;i--) {
+        lseek(fd, 3, SEEK_CUR);
+        read(fd, buffer, i);
+        printf("%s\n", buffer);
+    }
 }
 
 /*
