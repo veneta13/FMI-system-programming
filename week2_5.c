df48901ce885a21b23891b121b0272b18b13cb89
Veneta <36812226+veneta13@users.noreply.github.com>
2022-10-18 10:45:50 +0300

    Add week 2 - task 5

---
+++ week2_5.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+char buffer[16];
+int main() {
+    int fd=open("aa", O_RDONLY);
+    int i;
+    for(i=5; i>0; i--) {
+        execlp("wc", "wc", "-c", "aa", 0);
+    }
+}
