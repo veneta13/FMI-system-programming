06646a9f30925424691927071042a57e82dd074d
Veneta <36812226+veneta13@users.noreply.github.com>
2022-10-18 10:35:37 +0300

    Add week 3 - trace 2

---
+++ week3_tr2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main()
+{
+    int pid;
+    int b = 1, a = 5, status;
+    char buff[1];
+    pid = fork();
+    if ( pid == 0 ) {
+        b = 3;
+        a = 0;
+    }
+    else { 
+        a = 9;
+        wait(&status);
+    }
+    
+    write(1, "\n a= ", 5);
+    sprintf(buff, "%d", a);
+    write(1, buff, sizeof(buff));
+    
+    write(1, "\n b= ", 5);
+    sprintf(buff, "%d", b);
+    write(1, buff, sizeof(buff));
+}
