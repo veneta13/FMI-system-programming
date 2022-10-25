9af05a2fc90db39e9e993a874d7986464e9098ce
Veneta <36812226+veneta13@users.noreply.github.com>
2022-10-25 10:57:56 +0300

    Add week 1 - task 2

---
+++ week1_2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <fcntl.h>
+
+main(argc, argv)
+int argc;
+char *argv[];
+{
+    int flg = 0;
+
+    while (argc-- > 1)
+        ct(argv[++flg]);
+
+    if (flg == 0)
+        ct((char *)0);  /* ct (0); */
+}
+
+ct(s)
+char *s;
+{
+    char buffer[BUFSIZ];
+    int length; int fd =0;
+
+    if(s)
+        if( (fd=open(s,O_RDONLY)) == -1) {
+        printf("\ncat: can't open %s\n",s);
+        exit(1);
+    }
+    while(length = read(fd, buffer, BUFSIZ))
+        write(1, buffer, length);
+ }
