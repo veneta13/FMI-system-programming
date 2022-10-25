99852aaa538877badd0cf19d5cd471cc1ecac9cb
Veneta <36812226+veneta13@users.noreply.github.com>
2022-10-25 11:01:55 +0300

    Add week 1 - task 4

---
+++ week1_4.c
@@ -0,0 +1,30 @@
+#define YES 1
+#define NO 0
+
+#include <stdio.h>
+#include <fcntl.h>
+
+main(argc, argv)
+int argc;
+char *argv[];
+{
+    char c;
+    int fd, nl, nw, nc, inword;
+    inword = NO;
+    nl = nw = nc = 0;
+  
+    if((fd = open(argv[1], O_RDONLY)) == -1)
+        write(1, "error\n", 6);
+    while( (read(fd, &c, 1)) > 0){
+        nc++;
+        if ( c == '\n')
+            nl++;
+        if ( c  == ' ' || c =='\n' || c  == '\t')
+            inword = NO;
+        else if(inword == NO) {
+            inword = YES;
+            nw++;
+        }
+    }
+    printf("%d %d %d\n", nl, nw, nc);
+}
