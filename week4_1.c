4d9ae98e9478d19c662589b9ae0fb8f394ffa8f0
Veneta <36812226+veneta13@users.noreply.github.com>
2022-11-08 10:34:24 +0200

    Add week 4 - task 1

---
+++ week4_1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <fcntl.h>
+
+main() {
+    char buff[13];
+    int fd, fd1;
+
+    fd = open("ff", O_RDONLY);
+    fd1 = dup(fd);
+    read(fd, buff, 4);
+    write(1, buff, 4);
+    close(fd);
+    
+    read(fd1, buff, 9);
+    write(1, buff, 9);
+}
