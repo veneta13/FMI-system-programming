36329f0ea601e1480d3543f51604f84a20bc45f2
Veneta <36812226+veneta13@users.noreply.github.com>
2022-11-15 10:28:28 +0200

    Add week 6 - task 1

---
+++ week6_1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#define ERROR -1
+
+main()
+{   
+    int i;
+    if ( fork()==0 ) {
+        printf("child --> pid= %d ppid= %d\n", getpid(), getppid());
+        // for (i=1;i<= 1000000000 ;i++)  ;
+    }
+    else {
+        // wait();
+        printf("parent --> pid= %d, ppid= %d\n", getpid(), getppid());
+    }
+    
+    printf("\n Common part of parent+child\n");
+}
