5a82f49a5530c061c9f40791713819fd70deeadc
Veneta <36812226+veneta13@users.noreply.github.com>
2022-11-22 11:19:00 +0200

    Add week 7 - task 6

---
+++ week_07/week7_6.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <fcntl.h>
+
+#define ERROR -1
+
+main(int argc, char * argv[])
+{
+	int i, fd1, fd2, status;
+    if ((fd1=open(argv[1], O_WRONLY|O_CREAT|O_TRUNC, 0777)) == -1) {
+        printf("ERROR\n");
+        exit(1);
+    }
+    
+    printf("%d\n", fd1);
+    
+    if ((fd2=open(argv[2], O_WRONLY|O_CREAT|O_TRUNC, 0777)) == -1) {
+        printf("ERROR\n");
+        exit(1);
+    }
+    
+    printf("%d\n", fd2);
+    
+    if ( fork()==0 ) {
+        i=dup(1);
+        close(1);
+        dup(fd2);
+        write(1, "HELLO1", 6);
+        close(1);
+        dup(i);
+        //write(1, "HELLO2", 6);
+    }
+    else {
+        wait(&status);
+        write(fd1, "HELLO2", 6);
+    }
+    
+    write(1, "HELLO3", 6);
+    write(fd2, "HELLO4", 6);
+}
