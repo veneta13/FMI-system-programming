bde1d412e7798d018cf98f0b4295ce0f97a76c86
Veneta <venkireva80@gmail.com>
2022-11-25 22:27:49 +0200

    Reformat code

--- week_07/week7_9.c
+++ week_07/week7_9.c
@@ -6,20 +6,26 @@ main(int argc, char *argv[]) {
     int pid1, pid2, status1, status2, i = -1;
     if ((pid1 = fork()) > 0) {
         if ((pid2 = fork()) > 0) {
-            if (wait(&status1) == pid1)
-                if (status1 == 0)
+            if (wait(&status1) == pid1) {
+                if (status1 == 0) {
                     printf("%d\n", pid1);
+                }
                 else {
                     wait(&status2);
-                    if (status2 == 0)
+                    if (status2 == 0) {
                         printf("%d\n", pid2);
-                    if (status1 != 0 && status2 != 0)
+                    }
+                    if (status1 != 0 && status2 != 0) {
                         printf("%d\n", i);
+                    }
                 }
+            }
         }
-        else
+        else {
             execlp(argv[2], argv[2], 0);
+        }
     }
-    else
+    else {
         execlp(argv[1], argv[1], 0);
+    }
 }  
