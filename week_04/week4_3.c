2e8a2ebf2db7302baba8097c35f196f343437a5b
Veneta <venkireva80@gmail.com>
2022-11-25 18:09:37 +0200

    Revise week4_3.c

--- week_04/week4_3.c
+++ week_04/week4_3.c
@@ -1,24 +1,14 @@
-
-#include <stdio.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 main() {
-    int fd, i, k;
-    char str[8];
+    int fd = open("f1", O_CREAT | O_TRUNC | O_WRONLY, 0777);
+    close(1);
+    dup(fd);
 
-    for (i = 0; i <= 5; ++i)
-        printf("i = %d\n", i);
-    k = dup(1);
-
-    write(1, "duplicated descriptor k = ", sizeof("duplicated descriptor k = "));
-    sprintf(str, "%d", k);
-    write(1, str, strlen(str));
-    write(1, "\n", 1);
-
-    for (i = 0; i <= 10; ++i) {
-        sprintf(str, "i = %d\n", i);
-        write(k, str, strlen(str));
+    for (int i = 0; i < 10; i++) {
+        write(1, "EXAMPLE\n", 8);
     }
 }
