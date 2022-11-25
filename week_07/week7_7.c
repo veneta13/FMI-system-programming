4188b39fa66389ffee8cf21ccbc1bd68f82141a9
Veneta <venkireva80@gmail.com>
2022-11-25 22:17:29 +0200

    Add week 7 - task 7

---
+++ week_07/week7_7.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        write(2, "error: provide 2 filenames\n", sizeof("error: provide 2 filenames\n"));
+        exit(1);
+    }
+
+    int fd1 = open(argv[1], O_WRONLY | O_CREAT | O_TRUNC, 0777);
+    int fd2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0777);
+
+    if (fork() == 0) {
+        int stdOUT = dup(1);
+        close(1);
+        dup(fd2);
+
+        write(fd1, "hello2", 6);
+
+        close(1);
+        dup(stdOUT);
+        exit(0);
+    }
+    else {
+        write(fd1, "hello1", 6);
+        exit(0);
+    }
+
+    write(1, "hello3\n", 7);
+    write(fd2, "hello4", 6);
+}
