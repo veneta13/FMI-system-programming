999cbb388b75ddb3bbfb8564c088c2666eed4554
Veneta <venkireva80@gmail.com>
2022-11-25 12:59:20 +0200

    Add 'tee' command

--- week_01/week1_4.c
+++ week_01/week1_4.c
@@ -1,30 +1,31 @@
-#define YES 1
-#define NO 0
-
 #include <stdio.h>
 #include <fcntl.h>
+
+# define FILE_COUNT 10
+# define BUFSIZE 512
+
+// Program implementing the 'tee' command
 
 main(argc, argv)
         int argc;
         char *argv[];
 {
-    char c;
-    int fd, nl, nw, nc, inword;
-    inword = NO;
-    nl = nw = nc = 0;
 
-    if ((fd = open(argv[1], O_RDONLY)) == -1)
-        write(1, "error\n", 6);
-    while ((read(fd, &c, 1)) > 0) {
-        nc++;
-        if (c == '\n')
-            nl++;
-        if (c == ' ' || c == '\n' || c == '\t')
-            inword = NO;
-        else if (inword == NO) {
-            inword = YES;
-            nw++;
+    int fd[FILE_COUNT];
+    char buffer[BUFSIZE];
+    int numberBytes = 0;
+
+    for (int i = 1; i < argc; i++) {
+        if ((fd[i] = open(argv[i], O_CREAT | O_WRONLY | O_TRUNC, 0666)) == -1) {
+            printf("\nCannot creat %s", argv[i]);
+            exit(1);
         }
     }
-    printf("%d %d %d\n", nl, nw, nc);
+
+    while ((numberBytes = read(0, buffer, sizeof(buffer)))) {
+        write(1, buffer, numberBytes);
+
+        for (int i = 1; i < argc; i++)
+            write(fd[i], buffer, numberBytes);
+    }
 }
