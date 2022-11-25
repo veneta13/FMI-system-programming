216e6663bd041545833752837a870029ad11588b
Veneta <venkireva80@gmail.com>
2022-11-25 14:27:13 +0200

    Add actual week2_2

--- week_02/week2_2.c
+++ week_02/week2_2.c
@@ -1,19 +1,33 @@
 #include <unistd.h>
-#include <fcntl.h>
 #include <stdio.h>
+#include <fcntl.h>
 
 char buffer[16];
 
 int main() {
     int fd = open("aa", O_RDONLY);
     int i;
-    for (i = 5; i > 0; i--) {
-        lseek(fd, 3, SEEK_CUR);
-        read(fd, buffer, i);
-        printf("%s\n", buffer);
+
+    if (fd == -1) {
+        printf("error: can't open file\n");
+    }
+    else {
+        for (i = 5; i > 0; i--) {
+            int readBytes;
+
+            if ((readBytes = read(fd, buffer, i)) == 0) {
+                printf("The file has been read\n");
+            }
+
+            write(1, buffer, readBytes);
+        }
     }
 }
 
 /*
-  abcdefghijklmno
-*/
+ *
+ * Output:
+ *
+ * abcdefghijklmno
+ *
+ */
