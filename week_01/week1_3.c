bb34637d651f802ecaa7202fd87b521e22a2bd9e
Veneta <venkireva80@gmail.com>
2022-11-25 12:37:08 +0200

    Add 'cat >> file' command

--- week_01/week1_3.c
+++ week_01/week1_3.c
@@ -1,25 +1,42 @@
 #include <stdio.h>
-#include <sys/stat.h>
 #include <fcntl.h>
+#include <stdlib.h>
+
+# define BUFSIZE 512
+
+// Program implementing the 'cat >> file' command
 
 main(argc, argv)
         int argc;
         char *argv[];
 {
+    int fileCount = 0;
 
-    int fd[5], i;
-    char buff[10];
-    int nbytes = 0;
+    while (argc-- > 1)
+        cat(argv[++fileCount]);
 
-    for (i = 1; i < argc; i++) {
-        if ((fd[i] = open(argv[i], O_CREAT | O_WRONLY | O_TRUNC, 0666)) == -1) {
-            printf("\nCannot creat %s", argv[i]);
+    if (fileCount == 0) {
+        printf("cat: please provide files\n");
+        exit(1);
+    }
+
+    exit(0);
+}
+
+cat(s)
+        char *s;
+{
+    char buffer[BUFSIZE];
+    int length;
+    int fd;
+
+    if (s) {
+        if ((fd = open(s, O_RDONLY)) == -1) {
+            printf("\ncat: can't open %s\n", s);
             exit(1);
         }
     }
-    while ((nbytes = read(0, buff, sizeof(buff)))) {
-        write(1, buff, nbytes);
-        for (i = 1; i < argc; i++)
-            write(fd[i], buff, nbytes);
-    }
+
+    while ((length = read(fd, buffer, BUFSIZ)) > 0)
+        write(1, buffer, length);
 }
