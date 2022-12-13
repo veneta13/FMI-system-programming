f33455864a31cd66a35d7c48f9f55ff200a4943d
Veneta <36812226+veneta13@users.noreply.github.com>
2022-12-13 10:55:50 +0200

    Add week 11 - task 2

---
+++ week_11/week11_2.c
@@ -0,0 +1,29 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+main(int argc, char *argv[]) {
+    if (argc != 3) {
+        printf("Please provide 2 arguments!\n");
+        exit(1);
+    }
+    
+    int fd[2];
+    pipe(fd);
+    
+    if (fork()) {
+        close(fd[0]);
+        close(1);
+        dup(fd[1]);
+        close(fd[1]);
+        execlp(argv[1], argv[1], 0);
+    }
+    else {
+        close(fd[1]);
+        close(0);
+        dup(fd[0]);
+        close(fd[0]);
+        execlp(argv[2], argv[2], 0);
+    }
+}
