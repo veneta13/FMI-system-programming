f09f7a8f5925a9d328e8989ed30ef3e238294aeb
Veneta <36812226+veneta13@users.noreply.github.com>
2023-01-20 23:53:53 +0200

    Create week15_2.c

---
+++ week_15/week15_2.c
@@ -0,0 +1,22 @@
+#include <fcntl.h>
+
+main(int argc, char *argv[]) {
+    int fd, k, status;
+    
+    fd = open(argv[1], O_RDWR);
+    
+    if(fork()) {
+        wait(&status);
+        dup(fd);
+        write(1, "who\n", 4);
+        execlp("who", "who", "-u", 0); 
+        write (fd, " exam1\n",5);
+    }
+    else {
+        close(1);
+        dup(fd);
+        for(k = 0; k <= 3; k++)
+            write(1, "exam2\n", 6); 
+        }
+        execlp("pwd", "pwd", 0);
+}
