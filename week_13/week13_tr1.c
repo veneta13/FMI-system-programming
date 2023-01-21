7e1b0966c3a97a3b1190068cf3eab09311958211
Veneta <venkireva80@gmail.com>
2023-01-21 17:00:03 +0200

    Add output

--- week_13/week13_tr1.c
+++ week_13/week13_tr1.c
@@ -1,20 +1,33 @@
 #include <stdio.h>
 #include <fcntl.h>
+#include <stdlib.h>
 
 main(int args, char* argv[]) {
     int pid, var = 1, status;
 
     if ((pid = fork()) == 0) {
-        var += 2; 
+        var += 2;
         printf("\n Var = %d ", var);
-    } 
+    }
     else {
         wait(&status);
         var++;
-        printf("\n Var = %d ", var); 
+        printf("\n Var = %d ", var);
         exit(0);
     }
 
     --var;
     printf("\n Var = %d ", var);
 }
+
+/*
+ *
+ * Output:
+ *
+ *
+ * Var = 3
+ * Var = 2
+ * Var = 2
+ *
+ * /
+ *
