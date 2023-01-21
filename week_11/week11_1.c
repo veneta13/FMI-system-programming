35183a760ac7333197cf1a8e2108a8bef54132f1
Veneta <venkireva80@gmail.com>
2023-01-21 15:31:40 +0200

    Clarify week 11 - task 1

--- week_11/week11_1.c
+++ week_11/week11_1.c
@@ -6,19 +6,20 @@
 main(int argc, char *argv[]) {
     int fd[2];
     pipe(fd);
-    
-    if (fork()) {
-        close(fd[0]);
-        close(1);
-        dup(fd[1]);
-        close(fd[1]);
-        execlp("who", "who", 0);
+
+    if (fork() == 0) {
+        close(fd[0]); // close unused pipe read fd
+        close(1); // close standard out
+        dup(fd[1]); // dup pipe write to standard out
+        close(fd[1]); // close the pipe write fd
+        execlp("who", "who", 0); // execute 'who' writing to pipe write fd
     }
     else {
-        close(fd[1]);
-        close(0);
-        dup(fd[0]);
-        close(fd[0]);
-        execlp("wc", "wc", "-l", 0);
+        wait(NULL); // wait for child
+        close(fd[1]); // close unused pipe write fd
+        close(0); // close standard in
+        dup(fd[0]); // dup pipe read to standard in
+        close(fd[0]); // close the pipe read fd
+        execlp("wc", "wc", "-l", 0); // execute 'wc' reading from pipe read fd
     }
 }
