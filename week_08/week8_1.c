4367345aa5c4cf767f247d11b46e60b352924508
Veneta <venkireva80@gmail.com>
2023-01-21 14:09:05 +0200

    Revisit week 8 - task 1

--- week_08/week8_1.c
+++ week_08/week8_1.c
@@ -1,36 +1,46 @@
+#include <fcntl.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 
-main() {
-    char cmd[32];
-    int i = 0;
+char buffer[50];
 
+int main() {
     while (1) {
-        write(1, "CI>", 3);
+        write(1, ">>> ", 4);
 
-        while((read(0, &cmd[i], 1)) && cmd[i] != '\n') {
-            if(cmd[i] == ' ')
+        int buff_cnt = 0;
+
+        while (read(0, &buffer[buff_cnt], 1) > 0) {
+            if (buffer[buff_cnt] == '\n') {
+                buffer[buff_cnt] = '\0';
+                break;
+            }
+
+            if (buffer[buff_cnt] == ' ') {
                 continue;
-            else
-                ++i;
+            }
+
+            buff_cnt++;
         }
 
-        if(cmd[i] == '\n') 
-            cmd[i] = '\0';
-
-        if(strcmp(cmd,"quit") == 0)
+        if (strcmp(buffer, "quit") == 0) {
             exit(0);
+        }
         else {
-            if(fork()) {
-                wait();
-                i=0;
+            if (fork() == 0) {
+                execlp(buffer, buffer, 0);
+                exit(1);
             }
             else {
-                if(execlp(cmd,cmd,0) < 0) {
-                    printf("cannot execute %s\n", cmd);
-                    exit(1);
+                int status;
+                wait(&status);
+
+                if (status != 0) {
+                    printf("ERROR: can't execute command\n");
                 }
             }
-        }   
+        }
     }
 }
